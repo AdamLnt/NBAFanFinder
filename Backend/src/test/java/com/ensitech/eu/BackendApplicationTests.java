5acb6e58bb7301c1b7f597347adfaf9e64c01d1d
Adam Lyonnet <adam.lyonnet@ebp.com>
2025-12-01 14:52:32 +0100

    first commit

---
+++ Backend/src/test/java/com/ensitech/eu/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensitech.eu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
